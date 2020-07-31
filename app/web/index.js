d850945cc186e7f43c7e05f67dd54f25742353b0
RyanSeebregts <38289632+RyanSeebregts@users.noreply.github.com>
2020-07-31 20:13:53 +0200

    asdasdas

---
+++ app/web/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+	<div style={{height: '50px', width: '50px', background: 'red'}}></div>
+	, document.getElementById('root'));
+
+	
